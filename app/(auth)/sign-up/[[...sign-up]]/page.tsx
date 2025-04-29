94eac1027f093c46d5e01dfa63739bb4a2ce51eb
johannes <johannes.snildal@gmail.com>
2025-04-29 03:37:43 +0200

    added load animations to sign-in & sign-up

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,24 @@
+'use client'
 import { SignUp } from "@clerk/nextjs";
+import { motion } from 'framer-motion';
+
+const authVariants = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 1, ease: 'easeInOut' },
+  },
+};
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-zinc-950">
-      <div><SignUp /></div>
-    </div>
+    <main>
+      <motion.div initial="hidden" animate="visible" variants={authVariants}>
+        <div className="flex justify-center items-center min-h-screen bg-zinc-950">
+          <div><SignUp /></div>
+        </div>
+      </motion.div>
+    </main>
   );
 }
