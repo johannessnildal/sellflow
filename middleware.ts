5d9110dced71c5a4957cf2bcd6f1650b0dc0ca25
johannes <johannes.snildal@gmail.com>
2025-04-29 02:56:30 +0200

    made application only require log-in to access the dashboard interface

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/'
+const isPrivateRoute = createRouteMatcher([
+  '/dashboard(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
+  if (isPrivateRoute(req)) {
     await auth.protect()
   }
 })
